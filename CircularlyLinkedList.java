85296e8ee02862ecc0e109cd4ca6c5aadfbcc0ea
“Mark <mark.hartnett@ucdconnect.ie>
2018-02-21 10:47:59 +0000

    Class for human player playing the game created, circularly linked list of players created missing a removal function

---
+++ CircularlyLinkedList.java
@@ -0,0 +1,93 @@
+/*
+ * Class used to create a circularly linked list of human players
+ * playing the game
+ */
+public class CircularlyLinkedList {
+
+    private static class Node{
+        //element of the position in the linked list
+        private Player player;
+
+        //reference to next node in the list
+        private Node next;
+
+        //creates a given element before a certain node
+        public Node(Player p, Node n){
+            player = p;
+            next = n;
+        }
+
+        //accessor methods
+        public Player getPlayer() {
+            return player;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        //mutator methods
+        public void setPlayer(Player p) {
+            player = p;
+        }
+
+        public void setNext(Node n) {
+            next = n;
+        }
+
+    }
+
+    //private Node head = null;
+    private Node tail = null;
+    private int size = 0;
+
+    //constructs a circularly linked list
+    public CircularlyLinkedList(){
+    }
+
+    //function returns the size of the circularly linked list
+    public int getSize() {
+        return size;
+    }
+
+    //function returns true if the circularly linked list is empty
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    //returns the first player in the circularly linked list
+    public Player first(){
+        if (isEmpty()){
+            return null;
+        }
+        return tail.getNext().getPlayer();
+    }
+
+    //returns the last player in the circularly linked list
+    public Player last(){
+        if(isEmpty()){
+            return null;
+        }
+        return tail.getPlayer();
+    }
+
+    //add a player to the start of the list
+    public void addFirst(Player p){
+        if(isEmpty()){
+            tail = new Node(p,null);
+            tail.setNext(tail);
+        }
+        else{
+            Node n = new Node(p,tail.getNext());
+            tail.setNext(n);
+        }
+        size++;
+    }
+
+    //add player to tail of list
+    public void addLast(Player p){
+        addFirst(p);
+        tail = tail.getNext();
+    }
+
+}
---
+++ Player.java
@@ -0,0 +1,21 @@
+/*
+*   The Player class is the class that contains the information of the
+*   person playing the game.
+*/
+public class Player {
+    //name of the human playing the game
+    private String playerName;
+
+    //the character token that the player has chosen to move around the room
+    private Character playerToken;
+
+    //boolean value that states whether or not it is the players turn
+    private boolean live;
+
+    //constructor of the player object
+    public Player(String playerName, Character playerToken){
+        this.playerName = playerName;
+        this.playerToken = playerToken;
+        this.live = false;
+    }
+}
